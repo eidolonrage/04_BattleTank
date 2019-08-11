70a7169226a422c655fd97c27981f4c5a517a626
NATHAN-PC\Nathan <huntingfalcon@gmail.com>
2019-08-11 19:19:23 -0400

    Episode 175. Creating Unreal Engine UI with UMG and C++: Created initial widget base class derived from UUserWidget.

---
+++ BattleTank/Source/BattleTank/Private/BattleTankUserWidgetBase.cpp
@@ -0,0 +1,4 @@
+// /***  2014 - 2017 Nathan Boole All Rights Reserved.** NOTICE:* All information contained herein is, and remains the property of Nathan Boole* and its suppliers, if any. The intellectual and technical concepts contained herein* are proprietary to Grenunny Productions and its suppliers and may be covered by U.S.* and Foreign Patents, patents in process, and are protected by trade secret or copyright law.* Dissemination of this information or reproduction of this material is strictly forbidden* unless prior written permission is obtained from Nathan Boole.*/
+
+#include "BattleTankUserWidgetBase.h"
+
---
+++ BattleTank/Source/BattleTank/Public/BattleTankUserWidgetBase.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "BattleTankUserWidgetBase.generated.h"
+/**
+*
+*/
+UCLASS()
+
+class BATTLETANK_API UBattleTankUserWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+
+
+};
